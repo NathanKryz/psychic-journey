b32feac42cca7acc25987c801d947b26d69c34e8
NathanKryz <112029666+NathanKryz@users.noreply.github.com>
2022-10-27 11:00:07 -0400

    created static routing for homepage

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-// Set up Handlebars.js engine with custom helpers
+
 const hbs = exphbs.create({ helpers });
 
 const sess = {
