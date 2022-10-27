a323d8e53ce655480c8b324be4850f54f2c16ea9
ConstantMotion1 <constan.fernando@hotmail.com>
2022-10-27 11:09:33 -0400

    added character and monster models

---
+++ models/Monster.js
@@ -0,0 +1,41 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Monster extends Model {}
+
+Monster.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        class: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        design: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        hitpoints: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+        },
+        attack: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+        },
+        defense: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+        }
+    }
+);
+
+module.exports = Monster;
