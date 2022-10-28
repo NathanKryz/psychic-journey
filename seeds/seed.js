9161ff998c087d3d5c9fe4fa9639e7c647323255
ConstantMotion1 <constan.fernando@hotmail.com>
2022-10-28 18:18:19 -0400

    created seed index

---
+++ seeds/seed.js
@@ -0,0 +1,23 @@
+const sequelize = require('../config/connection');
+const { User, Character, Monster, Hangman } = require('../models');
+
+const userData = require('./user.json');
+const characterData = require('./characters.json');
+const monsterData = require('./monster.json');
+const hangmanWords = require('./words.json');
+
+const seedDatabase = async () => {
+    await sequelize.sync({ force: true });
+
+    await User.bulkCreate(userData, {
+        individualHooks: true,
+        returning: true,
+    });
+    await Character.bulkCreate(characterData);
+    await Monster.bulkCreate(monsterData);
+    await Hangman.bulkCreate(hangmanWords);
+
+    process.exit(0);
+};
+
+seedDatabase();
