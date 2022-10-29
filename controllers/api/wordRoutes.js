528a3636161c7a507d35f80b0588a7b58955f432
ConstantMotion1 <constan.fernando@hotmail.com>
2022-10-29 17:38:49 -0400

    updated wordsroute

--- controllers/api/wordRoutes.js
+++ controllers/api/wordRoutes.js
@@ -2,9 +2,9 @@ const router = require('express').Router();
 // const { response } = require('express');
 const { Hangman } = require('../../models');
 
-router.get('/:monster_id', async (req, res) => {
+router.get('/:id', async (req, res) => {
     try {
-        const chosenWords = await Hangman.findByPk(req.params.monster_id);
+        const chosenWords = await Hangman.findByPk(req.params.id);
         if (!chosenWords) {
           res.status(404).json({ message: 'This word does not exist' });
         //   response.render('homepage');
@@ -18,7 +18,7 @@ router.get('/:monster_id', async (req, res) => {
 
 router.get('/', async (req, res) => {
     try {
-        const chosenWords = await Character.findAll();
+        const chosenWords = await Hangman.findAll();
         if (!chosenWords) {
           res.status(404).json({ message: 'This word does not exist' });
         //   response.render('homepage');
