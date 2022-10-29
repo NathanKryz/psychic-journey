408270d930c4ef622d00c8168ee60d448e973303
NathanKryz <112029666+NathanKryz@users.noreply.github.com>
2022-10-29 18:49:59 -0400

    added dynamic handlebar template fetching to character page

--- controllers/homeroute.js
+++ controllers/homeroute.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const {User, Character} = require('../models');
 
 router.get('/', async (req, res) => {
     console.log("Rendering homepage");
@@ -13,12 +14,19 @@ router.get('/', async (req, res) => {
 
 
 router.get('/characters', async (req, res) => {
+    try {
     console.log("Rendering character page");
+    const charData = await Character.findAll();
+    const characters = charData.map((character) => character.get({plain: true}));
     req.session.current_page = true;
     res.render('characterselect', {
+        characters,
         logged_in: req.session.logged_in,
         char_page: req.session.current_page, 
     });
+    } catch (err) {
+        res.status(500).json(err);
+    }
 });
 
 module.exports = router;
